69ed94d26dbad2ebc39686ff76a3fc8b250d302e
Hayden Bleasel <hello@haydenbleasel.com>
2026-05-03 14:12:40 -0700

    Fix tests

--- packages/iframely/tsup.config.ts
+++ packages/iframely/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   dts: true,
-  entry: ["src"],
+  entry: ["src/**/*.{ts,tsx}", "!src/**/__tests__/**"],
   format: ["cjs", "esm"],
   minify: true,
   sourcemap: false,
